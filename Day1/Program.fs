98bce3fa251eb66e05aaa0c3dcb04b5d6d3da21a
ts-sander.homan <ts-sander.homan@rakuten.com>
2021-12-01 17:32:22 +0900

    Added day 1

---
+++ Day1/Program.fs
@@ -0,0 +1,31 @@
+﻿
+open System.IO
+open System
+
+let input = File.ReadAllLines("input.txt") |> Array.map int
+
+let part1 () =
+  ((Int32.MaxValue, 0), input)
+  ||> Array.fold (fun (prev, count) item -> 
+    if item > prev then
+      (item, count + 1)
+    else
+      (item, count)
+  )
+  |> snd
+
+let part2 () =
+  ((Int32.MaxValue, 0), input |> Array.windowed 3 |> Array.map (Array.reduce (+)))
+  ||> Array.fold (fun (prev, count) item -> 
+    if item > prev then
+      (item, count + 1)
+    else
+      (item, count)
+  )
+  |> snd
+
+let part1Result = part1 ()
+printfn "part1: %d" part1Result
+
+let part2Result = part2 ()
+printfn "part2: %d" part2Result
